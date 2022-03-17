28d7cdff3ca8909f9d78013a23e99ade05d4cbe0
Samuel <oiranca@gmail.com>
2022-03-17 12:34:11 +0000

    fix directory and rename style

--- src/componets/atoms/ImageProfile.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import './imageProfile.scss'
-
-export const ImageProfile = (imageInformation) => {
-  const { src,alt } = imageInformation;
-  return (<img className={'image-profile'} src={src} alt={alt} />);
-};
