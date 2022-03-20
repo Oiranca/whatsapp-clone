f8d8246b46339df0a8c89f1ca1a8db555f919b90
Samuel <oiranca@gmail.com>
2022-03-20 12:30:42 +0000

    rename mobile components

--- src/componets/molecules/NavMobileTitle/NavMobileTitle.jsx
+++ src/componets/molecules/NavMobileTitle/NavMobileTitle.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-import "./navTitleBar.scss";
 import { TitleProfile } from "../../atoms/TitleProfile/TitleProfile";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BsThreeDotsVertical } from "react-icons/bs";
 
+import "./navMobileTitle.scss";
+
 const iconsStyle = { size: "1.5rem", color: "#F0F8FFFF" };
 
-export const NavTitleBar = () => {
+export const NavMobileTitle = () => {
   return (<article className={"nav-container"}>
     <TitleProfile title={"Whatsapp"} />
     <section className={"nav-bar-button"}>
